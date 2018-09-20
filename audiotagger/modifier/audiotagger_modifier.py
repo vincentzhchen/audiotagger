0003a300c2479ab3392f5607b27b2fd234b368f4
Vincent Chen <vincent.zh.chen@gmail.com>
2018-09-20 01:44:03 -0400

    added some tag modifiers

--- audiotagger/modifier/audiotagger_modifier.py
+++ audiotagger/modifier/audiotagger_modifier.py
@@ -1,4 +1,39 @@
 class AudioTaggerModifier(object):
-    def __init__(self, logger, input):
-        self.log    = logger
-        self.input  = input
+    @staticmethod
+    def remove_leading_trailing_spaces(var):
+        """Removes leading trailing spaces from string tags.
+
+        Args:
+            var (str): TITLE, ARTIST, etc.
+
+        Returns:
+            anonymous (str): Stripped string.
+        """
+        return var.strip()
+
+    @staticmethod
+    def remove_multiple_whitespace(var):
+        """Removes multiple whitespaces in the middle of a string.
+
+        This implementation also removes all whitespace characters (e.g.
+        tab, newline, return, etc.)
+
+        Args:
+            var (str): TITLE, ARTIST, etc.
+
+        Returns:
+            anonymous (str): Returns clean string.
+        """
+        return " ".join(var.split())
+
+    @staticmethod
+    def uppercase(var):
+        return var.upperc()
+
+    @staticmethod
+    def lowercase(var):
+        return var.lower()
+
+    @staticmethod
+    def title(var):
+        return var.title()
