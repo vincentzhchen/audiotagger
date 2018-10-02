10c24510e2865006165b70599beb8156d2db0ca5
Vincent Chen <vincent.zh.chen@gmail.com>
2018-10-02 00:41:16 -0400

    cleaned up utils

--- audiotagger/utils/utils.py
+++ audiotagger/utils/utils.py
@@ -7,89 +7,57 @@ from audiotagger.core.paths import audiotagger_log_dir
 from audiotagger.data.fields import Fields as fld
 
 
-class AudioTaggerUtils(object):
+class FileUtils(object):
     def __init__(self):
         pass
 
-    @staticmethod
-    def get_file_extension(path_to_some_file):
+    @classmethod
+    def get_file_extension(cls, path_to_some_file):
         filename, file_extension = os.path.splitext(path_to_some_file)
         return file_extension
 
-    @staticmethod
-    def is_m4a(path_to_some_file):
-        file_extension = AudioTaggerUtils.get_file_extension(path_to_some_file)
+    @classmethod
+    def is_m4a(cls, path_to_some_file):
+        file_extension = FileUtils.get_file_extension(path_to_some_file)
         return True if file_extension == ".m4a" else False
 
-    @staticmethod
-    def is_mp3(path_to_some_file):
-        file_extension = AudioTaggerUtils.get_file_extension(path_to_some_file)
+    @classmethod
+    def is_mp3(cls, path_to_some_file):
+        file_extension = FileUtils.get_file_extension(path_to_some_file)
         return True if file_extension == ".mp3" else False
 
-    @staticmethod
-    def is_wav(path_to_some_file):
-        file_extension = AudioTaggerUtils.get_file_extension(path_to_some_file)
+    @classmethod
+    def is_wav(cls, path_to_some_file):
+        file_extension = FileUtils.get_file_extension(path_to_some_file)
         return True if file_extension == ".wav" else False
 
-    @staticmethod
-    def is_flac(path_to_some_file):
-        file_extension = AudioTaggerUtils.get_file_extension(path_to_some_file)
+    @classmethod
+    def is_flac(cls, path_to_some_file):
+        file_extension = FileUtils.get_file_extension(path_to_some_file)
         return True if file_extension == ".flac" else False
 
-    @staticmethod
-    def is_ape(path_to_some_file):
-        file_extension = AudioTaggerUtils.get_file_extension(path_to_some_file)
+    @classmethod
+    def is_ape(cls, path_to_some_file):
+        file_extension = FileUtils.get_file_extension(path_to_some_file)
         return True if file_extension == ".ape" else False
 
-    @staticmethod
-    def filter_m4a_files(arg):
+    @classmethod
+    def filter_m4a_files(cls, arg):
         if isinstance(arg, str):
             arg = [arg]
 
-        return [x for x in arg if AudioTaggerUtils.is_m4a(x)]
+        return [x for x in arg if FileUtils.is_m4a(x)]
 
-    @staticmethod
-    def apply_utf8(x):
+    @classmethod
+    def apply_utf8(cls, x):
         return x.encode("utf-8").decode("utf-8")
 
-    @staticmethod
-    def convert_to_mp4_obj(file_paths):
+    @classmethod
+    def convert_to_mp4_obj(cls, file_paths):
         return [MP4(path) for path in file_paths]
 
-    @staticmethod
-    def rename_columns(df):
-        return df.rename(columns=fld.ID3_to_field)
-
-    @staticmethod
-    def filter_by_artist(df, artist):
-        ret = df
-        return ret.loc[df[fld.ARTIST] == artist]
-
-    @staticmethod
-    def metadata_to_tags(df_metadata):
-        df_metadata[fld.TRACK_NUMBER] = df_metadata[
-            [fld.TRACK_NO, fld.TOTAL_TRACKS]].apply(tuple, axis="columns")
-        df_metadata[fld.DISC_NUMBER] = df_metadata[
-            [fld.DISC_NO, fld.TOTAL_DISCS]].apply(tuple, axis="columns")
-        df_metadata.drop([fld.TRACK_NO, fld.TOTAL_TRACKS,
-                          fld.DISC_NO, fld.TOTAL_DISCS],
-                         axis="columns", inplace=True)
-        df_metadata[fld.YEAR] = df_metadata[fld.YEAR].astype(str)
-        df_metadata = df_metadata.applymap(lambda x: [x])
-
-        tag_dict = {}
-        df_metadata.columns = [
-            fld.field_to_ID3.get(c, c) for c in df_metadata.columns]
-        metadata_dicts = df_metadata.to_dict(orient="records")
-        for d in metadata_dicts:
-            path = d.pop("PATH")[0]
-            tags = MP4Tags()
-            tags.update(d)
-            tag_dict.update({path: tags})
-        return tag_dict
-
-    @staticmethod
-    def traverse_directory(src):
+    @classmethod
+    def traverse_directory(cls, src):
         """Recursively traverses a directory.
 
         Notes:
@@ -119,8 +87,47 @@ class AudioTaggerUtils(object):
 
         return all_file_paths
 
-    @staticmethod
-    def dry_run(df, prefix=None):
+
+class TagUtils(object):
+    def __init__(self):
+        pass
+
+    @classmethod
+    def rename_columns(cls, df):
+        return df.rename(columns=fld.ID3_to_field)
+
+    @classmethod
+    def filter_by_artist(cls, df, artist):
+        ret = df
+        return ret.loc[df[fld.ARTIST] == artist]
+
+    @classmethod
+    def metadata_to_tags(cls, df_metadata):
+        df_metadata[fld.TRACK_NUMBER] = df_metadata[
+            [fld.TRACK_NO, fld.TOTAL_TRACKS]].apply(tuple, axis="columns")
+        df_metadata[fld.DISC_NUMBER] = df_metadata[
+            [fld.DISC_NO, fld.TOTAL_DISCS]].apply(tuple, axis="columns")
+        df_metadata.drop([fld.TRACK_NO, fld.TOTAL_TRACKS,
+                          fld.DISC_NO, fld.TOTAL_DISCS],
+                         axis="columns", inplace=True)
+        df_metadata[fld.YEAR] = df_metadata[fld.YEAR].astype(str)
+        df_metadata = df_metadata.applymap(lambda x: [x])
+
+        tag_dict = {}
+        df_metadata.columns = [
+            fld.field_to_ID3.get(c, c) for c in df_metadata.columns]
+        metadata_dicts = df_metadata.to_dict(orient="records")
+        for d in metadata_dicts:
+            path = d.pop("PATH")[0]
+            tags = MP4Tags()
+            tags.update(d)
+            tag_dict.update({path: tags})
+        return tag_dict
+
+    @classmethod
+    def dry_run(cls, df, prefix=None):
+        # TODO: this may not be something to abstract as each
+        #       implementation's dry run is not the same
         out_file = os.path.join(audiotagger_log_dir(),
                                 f"dry_run_{pdu.now(as_string=True)}.xlsx")
         if prefix is not None:
@@ -128,3 +135,8 @@ class AudioTaggerUtils(object):
                                     prefix + "_" +
                                     f"dry_run_{pdu.now(as_string=True)}.xlsx")
         df.to_excel(out_file, index=False)
+
+    @classmethod
+    def enforce_dtypes(cls, df):
+        df[fld.ALBUM] = df[fld.ALBUM].astype(str)
+        return df
