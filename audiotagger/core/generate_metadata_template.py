3d992e66745066aeab075f2364438c3cd3ca9aa7
Vincent Chen <vincent.zh.chen@gmail.com>
2018-10-15 02:45:50 -0400

    clean up metadata template

--- audiotagger/core/generate_metadata_template.py
+++ audiotagger/core/generate_metadata_template.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import pandasdateutils as pdu
 from audiotagger.core.paths import audiotagger_config_dir
 from audiotagger.data.fields import Fields as fld
 
@@ -9,11 +10,12 @@ def generate_metadata_template(dst_dir=None):
         dst_dir = audiotagger_config_dir()
 
     if os.path.isdir(dst_dir):
-        dst = os.path.join(dst_dir, "metadata.xlsx")
+        now = pdu.now(as_string=True)
+        dst = os.path.join(dst_dir, f"metadata_{now}.xlsx")
         print(f"Generating metadata file at {dst} ... ", end="")
 
         df = pd.DataFrame(columns=[fld.PATH] + fld.BASE_METADATA_COLS)
-        df.to_excel(dst, index=False)
+        df.to_excel(dst, sheet_name="metadata", index=False)
         print("done.")
     else:
         raise Exception(f"{dst_dir} does not exist.")
