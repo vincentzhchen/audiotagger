16352d6d7908f6d9c333e3fd791a628e509c1971
Vincent Chen <vincent.zh.chen@gmail.com>
2019-05-06 00:05:57 -0400

    add write utils

--- audiotagger/util/input_output_util.py
+++ audiotagger/util/input_output_util.py
@@ -1,13 +1,30 @@
+# STANDARD LIB
+import os
 import pandas as pd
+
+# DEPENDENCIES
+import pandasdateutils as pdu
+
+# PROJECT LIB
+from audiotagger.core import paths
+from audiotagger.data import fields
+
+# ALIAS
+fld = fields.Fields()
+
+
+def generate_excel_path(context):
+    base_dir = paths.audiotagger_data_dir()
+    context += f"_{pdu.now(as_string=True)}.xlsx"
+    file_path = os.path.join(base_dir, context)
+    return file_path
 
 
 def write_to_excel(df, file_path, sheet_name="metadata"):
     """Write dataframe to Excel file.
 
-    This is expected to be used for writing metadata dataframes.
-
     Args:
-        df (dataframe): Input dataframe.
+        df (pd.DataFrame): Input dataframe.
         file_path (str): Output file path to write the data to.
         sheet_name (str): Name of the dataframe.
 
@@ -22,3 +39,21 @@ def write_to_excel(df, file_path, sheet_name="metadata"):
                 index=False, encoding="utf-8")
 
     writer.save()
+
+
+def write_metadata_to_excel(df, file_path, ignore_cover=True):
+    """Write metadata dataframe to Excel file.
+
+    Args:
+        df (pd.DataFrame): Metadata dataframe.
+        file_path (str): Output file path to write data to.
+        ignore_cover (bool): If True, do not write the cover column,
+            which contains a bit stream value.
+
+    Returns:
+        void
+    """
+    if ignore_cover:
+        df = df.drop(columns=fld.COVER.CID, errors="ignore")
+
+    return write_to_excel(df=df, file_path=file_path, sheet_name="metadata")
