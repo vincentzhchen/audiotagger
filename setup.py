36c8708ccbee116b25030e5fa53e0bf7ce169eb7
vincentzhchen <vincent.zh.chen@gmail.com>
2020-05-09 16:45:03 -0400

    update setup, version bump

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ URL = "https://github.com/vincentzhchen/audiotagger"
 
 setup(
     name="audiotagger",
-    version="0.0.0",
+    version="0.0.1",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["pandas>=0.18.0,<0.24.0",
-                      "mutagen>=1.41.1",
-                      "redquill"],
+    install_requires=[
+        "pandas>=0.18.0", "mutagen>=1.41.1", "openpyxl>2.5.11", "redquill"
+    ],
     setup_requires=["pytest-runner"],
     tests_require=["pytest-cov"],
     python_requires=">=3.6",
@@ -24,19 +24,16 @@ setup(
     description="Cross-platform metadata editor for audio files.",
     long_description=README,
     long_description_content_type="text/markdown",
-
     author="Vincent Chen",
     author_email="vincent.zh.chen@gmail.com",
     url=URL,
-    project_urls={
-        "Source": URL
-    },
+    project_urls={"Source": URL},
     classifiers=[
         "License :: OSI Approved :: "
         "GNU General Public License v3 or later (GPLv3+)",
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Topic :: System :: Logging",
+        "Topic :: Multimedia :: Sound/Audio",
     ],
 )
