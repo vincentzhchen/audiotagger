778da726ca46e345098af0f2c723966af6bcd353
vincentzhchen <vincent.zh.chen@gmail.com>
2020-05-14 02:01:53 -0400

    more test

---
+++ audiotagger/tests/api/test_api.py
@@ -0,0 +1,24 @@
+# STANDARD LIB
+import os
+import pandas as pd
+import pathlib
+import pytest
+
+# PROJECT LIB
+from audiotagger.api import api
+from audiotagger.data import fields as fld, input as at_in
+from audiotagger.util import audiotagger_logger
+
+logger = audiotagger_logger.get_logger(name="test_audiotagger_api.log")
+
+
+@pytest.fixture
+def api_instance():
+    test_dir = pathlib.Path(__file__).parent.parent
+    src = os.path.join(test_dir, "sample_data/test_metadata.xlsx")
+    at = api.AudioTaggerAPI(src=src)
+    return at
+
+
+def test_dry_run(api_instance):
+    api_instance.run()
